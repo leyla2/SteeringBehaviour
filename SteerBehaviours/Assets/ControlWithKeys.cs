dc5c71220626c7b1c2f451609026029ade4f7e57
Leyla Asan Mane <lasan001@gold.ac.uk>
2017-12-26 23:44:08 +0100

    added car, adjusted camera, adjusted terrain, added movement script

--- SteerBehaviours/Assets/ControlWithKeys.cs
+++ SteerBehaviours/Assets/ControlWithKeys.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 
 public class ControlWithKeys : MonoBehaviour {
-    Vector3 speed = new Vector3(5, 1, 0);// was testing movement with this
+    Vector3 velo = new Vector3(5, 1, 0);// was testing movement with this
+    public float speed; 
 	// Use this for initialization
 	void Start () {
-	
+        speed = 10;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate((Input.GetAxis("Horizontal") * Time.deltaTime)*5, 0f, (Input.GetAxis("Vertical") * Time.deltaTime)*5);// get the wasd or UP/DOWN/LEFT/RIGHT input and move 1 pixel accordingly, also in relation to your system's time.
+        transform.Translate((Input.GetAxis("Horizontal") * Time.deltaTime)*speed, 0f, (Input.GetAxis("Vertical") * Time.deltaTime)*speed);// get the wasd or UP/DOWN/LEFT/RIGHT input and move 1 pixel accordingly, also in relation to your system's time.
 	}
 }
