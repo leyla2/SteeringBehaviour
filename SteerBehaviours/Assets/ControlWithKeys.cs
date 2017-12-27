5919555d4f7ba487242f2425633209896f56b4ad
Leyla Asan Mane <lasan001@gold.ac.uk>
2017-12-27 22:10:26 +0100

    added box collision

--- SteerBehaviours/Assets/ControlWithKeys.cs
+++ SteerBehaviours/Assets/ControlWithKeys.cs
@@ -6,11 +6,13 @@ public class ControlWithKeys : MonoBehaviour {
     public float speed; 
 	// Use this for initialization
 	void Start () {
-        speed = 10;
+        speed = 100;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.Translate((Input.GetAxis("Horizontal") * Time.deltaTime)*speed, 0f, (Input.GetAxis("Vertical") * Time.deltaTime)*speed);// get the wasd or UP/DOWN/LEFT/RIGHT input and move 1 pixel accordingly, also in relation to your system's time.
-	}
+        transform.Rotate(0f,0f, Input.GetAxis("Horizontal") * Time.deltaTime);// get the wasd or UP/DOWN/LEFT/RIGHT input and move 1 pixel accordingly, also in relation to your system's time.
+
+    }
 }
