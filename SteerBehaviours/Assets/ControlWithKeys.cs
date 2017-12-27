ead8d8498e954d9212ff64e49d624a993f34f5db
Leyla Asan Mane <lasan001@gold.ac.uk>
2017-12-27 23:42:14 +0100

    Added rotation to car y axis

--- SteerBehaviours/Assets/ControlWithKeys.cs
+++ SteerBehaviours/Assets/ControlWithKeys.cs
@@ -12,7 +12,8 @@ public class ControlWithKeys : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.Translate((Input.GetAxis("Horizontal") * Time.deltaTime)*speed, 0f, (Input.GetAxis("Vertical") * Time.deltaTime)*speed);// get the wasd or UP/DOWN/LEFT/RIGHT input and move 1 pixel accordingly, also in relation to your system's time.
-        transform.Rotate(0f,0f, Input.GetAxis("Horizontal") * Time.deltaTime);// get the wasd or UP/DOWN/LEFT/RIGHT input and move 1 pixel accordingly, also in relation to your system's time.
+        transform.Rotate(0f,0f, Input.GetAxis("Horizontal") * Time.deltaTime * 200);// on turn give the car a realistic 'falling over' effect. 
+        transform.Rotate(0f, Input.GetAxis("Horizontal") * Time.deltaTime * 100, 0f);
 
     }
 }
