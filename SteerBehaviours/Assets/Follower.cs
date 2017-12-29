d7365eda00aece94e606600430f851637976f107
Leyla Asan Mane <lasan001@gold.ac.uk>
2017-12-29 19:49:31 +0100

    added steering behaviour ai draft

---
+++ SteerBehaviours/Assets/Follower.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follower : MonoBehaviour {
+    public Transform car;
+    public Transform chicken;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(car);
+        transform.Translate(Vector3.forward * 5 * Time.deltaTime);
+    }
+}
